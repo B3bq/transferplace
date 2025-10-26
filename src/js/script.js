84cc31644fee97e01c4eaddc2561347c2da8a398
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2025-10-26 19:58:45 +0100

    stats update

--- src/js/script.js
+++ src/js/script.js
@@ -201,7 +201,7 @@ document.addEventListener("DOMContentLoaded", () => {
     viewLeague.style.display = "grid";
     viewPuchar.style.display = "none";
     leagueName.innerHTML = "A Klasa";
-    leagueMatches.innerHTML = "10 możliwych meczów";
+    leagueMatches.innerHTML = "11 możliwych meczów";
   }
 
   function showPuchar() {
