195c6f8b51bc0c8b572dc86beea740d3494e2402
B3bq <sebastian.kaca@poczta.onet.pl>
2026-05-16 20:19:22 +0200

    win

--- src/js/script.js
+++ src/js/script.js
@@ -201,7 +201,7 @@ document.addEventListener("DOMContentLoaded", () => {
     viewLeague.style.display = "grid";
     viewPuchar.style.display = "none";
     leagueName.innerHTML = "A Klasa";
-    leagueMatches.innerHTML = "19 możliwych meczów";
+    leagueMatches.innerHTML = "20 możliwych meczów";
   }
 
   function showPuchar() {
